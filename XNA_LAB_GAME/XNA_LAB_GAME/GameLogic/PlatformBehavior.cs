229729f11b6759176bb85634b8e34d3f17a00257
Alexander Kostenko <sanyokkua@gmail.com>
2015-03-30 19:51:07 +0300

    Working version

---
+++ XNA_LAB_GAME/XNA_LAB_GAME/GameLogic/PlatformBehavior.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using XNA_LAB_GAME.GameLogic;
+
+namespace XNA_LAB_GAME.GameLogic
+{
+    class PlatformBehavior: IBehaviore
+    {
+        private Vector2 velocity;
+        private Vector2 position;
+
+        public PlatformBehavior(float velocity = 0)
+        {
+            if (velocity < 0 || velocity > 1) { velocity = 0.0f; }
+            this.velocity = new Vector2(velocity);
+            position = new Vector2();
+        }
+        public void Update(ref object obj)
+        {
+            Rectangle rec;
+            Texture2D text;
+            int width;
+            int height;
+            try
+            {
+                rec = ((MovebleGameObject)obj).RECTANGLE;
+                text = ((MovebleGameObject)obj).TEXTURE;
+
+                width = ((MovebleGameObject)obj).MAX_WIDTH;
+                height = ((MovebleGameObject)obj).MAX_HEIGHT;
+            }
+            catch { return; }
+            rec.Y += (int)(velocity.Y);
+            ((MovebleGameObject)obj).RECTANGLE = rec;
+        }
+
+        public void Intersect(ref object obj, ref GameObject gameObject)
+        {
+            MovebleGameObject mObj;
+            try
+            {
+                mObj = ((MovebleGameObject)obj);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            if (mObj.RECTANGLE.Intersects(gameObject.RECTANGLE))
+            {
+                if (mObj.ObjectState == GameObject.STATE.GOOD)
+                {
+                    mObj.ObjectState = GameObject.STATE.MIDDLE;
+                    mObj.TEXTURE = mObj.OTHER_TEXTURES.ElementAt(0);
+                }
+                else if (mObj.ObjectState == GameObject.STATE.MIDDLE)
+                {
+                    mObj.ObjectState = GameObject.STATE.WORSE;
+                    mObj.TEXTURE = mObj.OTHER_TEXTURES.ElementAt(1);
+                }
+                else
+                {
+                    mObj.IS_VISIBLE = false;
+                    Song sound = mObj.SOUND;
+                    if (sound != null)
+                    {
+                        MediaPlayer.Stop();
+                        MediaPlayer.Play(sound);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ XNA_LAB_GAME/XNA_LAB_GAME/GameLogic/PlayerBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNA_LAB_GAME.GameLogic
+{
+    class PlayerBehavior : IBehaviore
+    {
+        private Vector2 position;
+        private int velocity;
+
+        public PlayerBehavior(int velocity = 3)
+        {
+            if (velocity > 0)
+            {
+                this.velocity = velocity;
+            }
+            else
+            {
+                velocity = 3;
+            }
+            position = new Vector2();
+        }
+        public void Update(ref object obj)
+        {
+            Rectangle rec;
+            Texture2D text;
+            int width;
+            int height;
+            try
+            {
+                rec = ((MovebleGameObject)obj).RECTANGLE;
+                text = ((MovebleGameObject)obj).TEXTURE;
+                width = ((MovebleGameObject)obj).MAX_WIDTH;
+                height = ((MovebleGameObject)obj).MAX_HEIGHT;
+                position.X = ((MovebleGameObject)obj).RECTANGLE.X;
+                position.Y = ((MovebleGameObject)obj).RECTANGLE.Y;
+            }
+            catch { return; }
+
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Left))
+            {
+                position.X-=velocity;
+            }
+            if (state.IsKeyDown(Keys.Right))
+            {
+                position.X+=velocity;
+            }
+
+            rec.X = (int)position.X;
+            rec.Y = (int)position.Y;
+            if (rec.X <= 0) { position.X = 0; }
+            if (rec.Y <= 0) { position.Y = 0; }
+            if (rec.X + rec.Width >= width) { position.X = width - rec.Width; }
+            if (rec.Y + rec.Height >= height) { position.Y = height - rec.Height; }
+            rec.X = (int)position.X;
+            rec.Y = (int)position.Y;
+            ((MovebleGameObject)obj).RECTANGLE = rec;
+        }
+        public void Intersect(ref object obj, ref GameObject gameObject) { }
+    }
+}
